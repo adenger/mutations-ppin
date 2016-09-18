411c2486c69f003dfd43c278b3d678f6eb85bff5
Andreas Denger <a.denger1@gmail.com>
2016-09-18 15:26:33 +0200

    continued with script

--- scripts/Print stats/printstats.py
+++ scripts/Print stats/printstats.py
@@ -1,6 +1,7 @@
 import os
-
+from collections import defaultdict
 #take protein connections: sort mutations that delete it by number of classfiier values that predicted it 
+interactions = defaultdict(lambda :defaultdict(list)) # uniprot pairs to list of mutation pairs 
 for filename in os.listdir():
 	if not filename.endswith(".txt"):
 		continue
@@ -9,4 +10,12 @@ for filename in os.listdir():
 			if not line.startswith("rs"):
 				continue
 			values = line.strip().split("\t")
-			print(values)
+			mutation = values[0]
+			mutProt = values[1]
+			intProt = values[2]
+			clVals = []
+			for i in range(3,12):
+				clVals.append(values[i])	
+			interactions[(mutProt,intProt)][mutation] =clVals
+	for (mutProt,intProt),mutations in interactions.items():
+		print(mutProt+"\t"+intProt+"\t"+str(len(mutations)))
